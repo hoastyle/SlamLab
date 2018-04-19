20da79d40029a61119ea5e46173b5b0c26835f9c
Hao Liang <hao.liang@nextvpu.com>
2018-04-19 20:06:54 +0800

    [NF]: add framework of tiny slam
    Signed-off-by: Hao Liang <hao.liang@nextvpu.com>

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Tiny Slam
+
+# Commit Rules
+Commit log should begin with one of following type names:
+[NF]: new feature
+[BF]: bug fix
---
+++ include/tinyslam/camera.h
@@ -0,0 +1,13 @@
+#ifndef __CAMERA_H_
+#define __CAMERA_H_
+
+#include "slam_h/common_include.h"
+
+namespace  slam_h
+{
+class Camera {
+public:
+}	
+}
+
+#endif
